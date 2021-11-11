56322f1759c186e5e6299d88bf1c89a9de9fd465
domi555 <66876360+domi555@users.noreply.github.com>
2021-11-11 19:16:26 +0100

    Update

---
+++ website/precache-manifest.98115f3a3a039a348fa4c66f84ef0884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67eba7ff4cfb79b00e1e",
+    "url": "/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d797e8932e05c01750b307a7c5d7a9ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67eba7ff4cfb79b00e1e",
+    "url": "/js/app.b7ea24a7.js"
+  },
+  {
+    "revision": "e4c9092601fc21c62af8",
+    "url": "/js/chunk-vendors.a964916c.js"
+  },
+  {
+    "revision": "4d9bc8460a2bdd2e99c21b63747c7a34",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
